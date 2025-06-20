8219e87a6929ae84e277a84284eb86f4f42a94e3
PCD010040\quan.dau <quan.dau@monstar-lab.com>
2025-06-20 10:16:30 +0700

    update rag_router

--- app/api/rag_router.py
+++ app/api/rag_router.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from app.service.rag_service import RagService
+from app.service.rag_service import rag_service
 
 router = APIRouter(prefix="/rag", tags=["RAG QA"])
 
 class Question(BaseModel):
     query: str
-
-rag_service = RagService()
-rag_service.load_model()
 
 @router.post("/ask")
 def ask_question(q: Question):
